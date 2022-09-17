56e01596cd53d3346b14fe0820fb390d382ac503
giu13sse <giussedipietro@gmail.com>
2022-09-16 19:02:57 -0700

    error

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -1,17 +1,16 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_delete - deletes an entire binary tree
- * @tree: pointer to the root node of the tree to delete
+ * binary_tree_is_leaf - checks if a node is a leaf
+ * @node: pointer to a node to check
  *
- * Return: void
+ * Return: 1 if the node is a leaf, 0 otherwise.
  */
-void binary_tree_delete(binary_tree_t *tree)
+int binary_tree_is_leaf(const binary_tree_t *node)
 {
-	if (tree)
+	if (node != NULL && node->left == NULL && node->right == NULL)
 	{
-		binary_tree_delete(tree->left);
-		binary_tree_delete(tree->right);
-		free(tree);
+		return (1);
 	}
+	return (0);
 }
